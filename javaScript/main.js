0c3742a71ea1ed49ca6cbd8048a998cc2f8475a6
Sarooz71 <162772901+Sarooz71@users.noreply.github.com>
2025-07-26 17:20:13 +0545

    Java Script made

---
+++ javaScript/main.js
@@ -0,0 +1,11 @@
+// console.log("Hello World");
+
+// Variable
+
+
+let name = "Saroj lama";
+let age = 21;
+let myplace = "Kirtipur";
+
+
+console.log(`My name is ${name}. I am ${age} years old. I live in ${myplace}.`)
