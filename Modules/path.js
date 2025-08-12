0fe2494462ee1b60df6a6f043fd3b84df17d8895
Sarooz71 <162772901+Sarooz71@users.noreply.github.com>
2025-08-12 19:41:06 +0545

    Update path

---
+++ Modules/path.js
@@ -0,0 +1,9 @@
+import path from "path";
+const filePath = "Assets\Samir Shrestha - Timi Nabhai Nahuney ( Official Raw Demo ).mp3";
+console.log(path.basename(filePath));
+
+console.log(path.dirname(filePath));
+
+console.log(path.extname(filePath));
+
+console.log(path.parse(filePath));
